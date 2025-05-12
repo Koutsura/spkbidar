e8635db54903895e6e2d1f23dcb33912fe007e46
Koutsura <m.denny.trilisandi@gmail.com>
2025-05-12 15:20:12 +0700

    update sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -26,40 +26,41 @@
                 <a class="nav-link" href="{{ url('hakakses') }}"><i class="fas fa-user"></i> <span>Edit Role</span></a>
             </li>
             @endif
+            @if (auth()->user()->role == 'rekomendasi' || auth()->user()->role == 'mahasiswa')
+             <li class="menu-header">Tes Rekomendasi UKM</li>
+             <li class="{{ Request::is('rekomendasi') ? 'active' : '' }}">
+                 <a class="nav-link" href="/"><i class="fas fa-newspaper"></i> <span>Rekomendasi UKM</span></a>
+             </li>
+              @endif
             <li class="menu-header">Profile</li>
             <li class="{{ Request::is('profile/edit') ? 'active' : '' }}">
                 <a class="nav-link" href="/"><i class="fas fa-user"></i> <span>Edit Profile</span></a>
             </li>
-            <li class="{{ Request::is('profile/ganti-password') ? 'active' : '' }}">
-                <a class="nav-link" href="/"><i class="fas fa-key"></i> <span>Ganti Password</span></a>
-            </li>
-            @if (auth()->user()->role == 'peserta' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'pendaftaran' || auth()->user()->role == 'superadmin')
             <li class="menu-header">Pages</li>
-            <li class="{{ Request::is('peserta') ? 'active' : '' }}">
-                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Data Mahasiswa</span></a>
+            <li class="{{ Request::is('pendaftaran') ? 'active' : '' }}">
+                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Pendaftaran</span></a>
             </li>
              @endif
-             @if (auth()->user()->role == 'program_studi' || auth()->user()->role == 'superadmin')
-            <li class="{{ Request::is('program_studi') ? 'active' : '' }}">
-                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Data Prodi</span></a>
-            </li>
-             @endif
-             @if (auth()->user()->role == 'perguruan_tinggi' || auth()->user()->role == 'superadmin')
-            <li class="{{ Request::is('perguruan_tinggi') ? 'active' : '' }}">
-                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Data Perguruan Tinggi</span></a>
-            </li>
-             @endif
-             @if (auth()->user()->role == 'seleksi' || auth()->user()->role == 'superadmin')
-            <li class="{{ Request::is('seleksi') ? 'active' : '' }}">
-                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Penetapan</span></a>
+             @if (auth()->user()->role == 'pelatihan' || auth()->user()->role == 'superadmin')
+            <li class="{{ Request::is('pelatihan') ? 'active' : '' }}">
+                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Pelatihan</span></a>
             </li>
              @endif
 
-             @if (auth()->user()->role == 'pencairan' || auth()->user()->role == 'superadmin')
-             <li class="{{ Request::is('pencairan') ? 'active' : '' }}">
-                 <a class="nav-link" href="/"><i class="fas fa-newspaper"></i> <span>Data Mahasiswa Eligible</span></a>
-             </li>
-              @endif
+             @if (auth()->user()->role == 'pendaftaran' || auth()->user()->role == 'mahasiswa')
+             <li class="menu-header">Pages</li>
+            <li class="{{ Request::is('pendaftaran') ? 'active' : '' }}">
+                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Pendaftaran</span></a>
+            </li>
+             @endif
+             @if (auth()->user()->role == 'pelatihan' || auth()->user()->role == 'mahasiswa')
+            <li class="{{ Request::is('pelatihan') ? 'active' : '' }}">
+                <a class="nav-link" href="/"><i class="fas fa-university"></i> <span>Pelatihan</span></a>
+            </li>
+             @endif
+
+
 
         </ul>
     </aside>
