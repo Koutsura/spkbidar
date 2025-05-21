7645960dfe19dda8d57d5c2b9e4def1380d71261
Koutsura <m.denny.trilisandi@gmail.com>
2025-05-21 20:55:36 +0700

    update header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,44 +1,50 @@
+<style>
+  /* Hapus custom CSS, biarkan Bootstrap yang mengatur */
+</style>
+
 <div class="navbar-bg"></div>
 <nav class="navbar navbar-expand-lg main-navbar">
-    <ul class="navbar-nav ml-auto">
-        @auth
+    <ul class="navbar-nav">
         <li>
             <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg">
                 <i class="fas fa-bars"></i>
             </a>
         </li>
+    </ul>
+
+    <ul class="navbar-nav ms-auto"><!-- ini untuk dropdown user di kanan -->
+        @auth
         <li class="nav-item dropdown">
-   <a href="#" id="navbarDropdown" role="button"
-   class="nav-link dropdown-toggle nav-link-lg nav-link-user"
-   data-bs-toggle="dropdown" aria-expanded="false">
-    <!-- Menampilkan foto profil dari database atau default -->
-    <img alt="image"
-         src="{{ auth()->user()->setting && auth()->user()->setting->profile_photo
-                ? asset('storage/' . auth()->user()->setting->profile_photo)
-                : asset('img/profile.png') }}"
-         class="rounded-circle mr-1" style="width: 35px; height: 35px; object-fit: cover;">
-    <div class="d-sm-none d-lg-inline-block">
-        Hai, {{ auth()->check() ? substr(auth()->user()->name, 0, 10) : 'Tamu' }}
-    </div>
-</a>
+            <a href="#" id="navbarDropdown" role="button"
+               class="nav-link dropdown-toggle nav-link-lg nav-link-user"
+               data-bs-toggle="dropdown" aria-expanded="false">
+                <img alt="image"
+                     src="{{ auth()->user()->setting && auth()->user()->setting->profile_photo
+                            ? asset('storage/' . auth()->user()->setting->profile_photo)
+                            : asset('img/profile.png') }}"
+                     class="rounded-circle me-1"
+                     style="width: 35px; height: 35px; object-fit: cover;">
+                <div class="d-sm-none d-lg-inline-block">
+                    Hai, {{ auth()->check() ? substr(auth()->user()->name, 0, 10) : 'Tamu' }}
+                </div>
+            </a>
 
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><h6 class="dropdown-header">Selamat Datang, {{ substr(auth()->user()->name, 0, 10) }}</h6></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <a href="{{ route('logout') }}" class="dropdown-item text-danger"
+                     onclick="event.preventDefault(); localStorage.clear(); document.getElementById('logout-form').submit();">
+                      <i class="fas fa-sign-out-alt"></i> Logout
+                  </a>
+                </li>
+            </ul>
 
-
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <div class="dropdown-title">
-            Selamat Datang, {{ auth()->check() ? substr(auth()->user()->name, 0, 10) : 'Tamu' }}
-        </div>
-        <div class="dropdown-divider"></div>
-        <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
-           onclick="event.preventDefault(); localStorage.clear(); document.getElementById('logout-form').submit();">
-            <i class="fas fa-sign-out-alt"></i> Logout
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
-    </div>
-</li>
-
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </li>
         @endauth
     </ul>
 </nav>
+
