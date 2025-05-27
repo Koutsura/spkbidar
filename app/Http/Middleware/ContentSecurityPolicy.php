f66876944e839f0ecc5a648b81c01b9d79f58996
Koutsura <m.denny.trilisandi@gmail.com>
2025-05-27 14:35:02 +0700

    update

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -13,17 +13,18 @@ class ContentSecurityPolicy
         $response = $next($request);
 
         $csp = "
-            default-src 'self' https: data:;
-            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://maps.googleapis.com https://maps.gstatic.com;
-            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdnjs.cloudflare.com https://cdn.jsdelivr.net;
-            img-src 'self' data: https://maps.gstatic.com https://maps.googleapis.com https://maps.google.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://ui-avatars.com;
-            font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net https://cdnjs.cloudflare.com;
-            connect-src 'self' https://maps.googleapis.com;
-            frame-src 'self' https://www.google.com https://maps.google.com https://www.google.com/maps;
-            frame-ancestors 'none';
-            base-uri 'self';
-            form-action 'self';
-        ";
+    default-src 'self' https: data:;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://maps.googleapis.com https://maps.gstatic.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdnjs.cloudflare.com https://cdn.jsdelivr.net;
+    img-src 'self' data: https://maps.gstatic.com https://maps.googleapis.com https://maps.google.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://ui-avatars.com;
+    font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net https://cdnjs.cloudflare.com;
+    connect-src 'self' https://maps.googleapis.com;
+    frame-src 'self' https://www.google.com https://maps.google.com https://www.google.com/maps;
+    frame-ancestors 'none';
+    base-uri 'self';
+    form-action 'self' https:;
+";
+
 
         // Bersihkan spasi dan newline menjadi satu spasi
         $csp = preg_replace('/\s+/', ' ', trim($csp));
