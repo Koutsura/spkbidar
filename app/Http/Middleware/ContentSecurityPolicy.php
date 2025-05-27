9394ca6dbbf820d87a7b446966839d42e02c4457
Koutsura <m.denny.trilisandi@gmail.com>
2025-05-27 13:46:01 +0700

    update cSP

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -18,11 +18,11 @@ class ContentSecurityPolicy
             style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdnjs.cloudflare.com https://cdn.jsdelivr.net;
             img-src 'self' data: https://maps.gstatic.com https://maps.googleapis.com https://maps.google.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://ui-avatars.com;
             font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net https://cdnjs.cloudflare.com;
-            connect-src 'self' https://maps.googleapis.com;
+            connect-src 'self' https://maps.googleapis.com https://ukmbidar.decadev.tech/;
             frame-src 'self' https://www.google.com https://maps.google.com https://www.google.com/maps;
             frame-ancestors 'none';
             base-uri 'self';
-            form-action 'self';
+            form-action 'self' https://ukmbidar.decadev.tech/;
         ";
 
         // Bersihkan spasi dan newline menjadi satu spasi
@@ -30,7 +30,7 @@ class ContentSecurityPolicy
 
         $response->headers->set('Content-Security-Policy', $csp);
         $response->headers->set('X-Content-Type-Options', 'nosniff');
-        $response->headers->set('X-Frame-Options', 'DENY'); // Kalau butuh iframe eksternal bisa diubah
+        $response->headers->set('X-Frame-Options', 'DENY'); // Ubah kalau perlu iframe eksternal
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
         $response->headers->set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
