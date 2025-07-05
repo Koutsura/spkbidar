fc14322a34a25232cb764c8a6cc893e94a6f3f60
Koutsura <m.denny.trilisandi@gmail.com>
2025-07-05 16:47:05 +0700

    update pop up UKM dashboard mahasiswa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,103 +14,97 @@
             <h1>Dashboard</h1>
         </div>
         <h2 class="mb-4">Selamat datang mahasiswa</h2>
-<div class="row">
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/bdca.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 1">
-            <div class="card-body p-2">
-                <p class="img-caption">Bina Darma Cyber Army(@bidarcyberarmy)</p>
-                <p class="text-muted small">UKM yang berfokus pada keamanan siber dan pelatihan ethical hacking.</p>
-            </div>
+
+        <div class="row">
+            @php
+                $ukms = [
+                    [
+                        'img' => 'bdca.webp',
+                        'title' => 'Bina Darma Cyber Army(@bidarcyberarmy)',
+                        'desc' => 'UKM yang berfokus pada keamanan siber dan pelatihan ethical hacking.'
+                    ],
+                    [
+                        'img' => 'qarib.webp',
+                        'title' => 'LDK ALQORIB(@ldkalqoribbidar)',
+                        'desc' => 'Lembaga dakwah kampus Islam yang membina rohani, akhlak, dan ukhuwah Islamiyah.'
+                    ],
+                    [
+                        'img' => 'PMKK.webp',
+                        'title' => 'Persekutuan Mahasiswa Kristen & Katolik(@pmkkubd)',
+                        'desc' => 'Wadah pengembangan iman dan kebersamaan bagi mahasiswa Kristen dan Katolik.'
+                    ],
+                    [
+                        'img' => 'kmhdi.webp',
+                        'title' => 'Kesatuan Mahasiswa Hindu Darma Indonesia(@kmhdipalembang)',
+                        'desc' => 'UKM keagamaan Hindu yang berfokus pada pembinaan spiritual dan budaya Hindu.'
+                    ],
+                    [
+                        'img' => 'mabidar.webp',
+                        'title' => 'Mahasiswa Pencinta Alam(@mabidar.palembang)',
+                        'desc' => 'UKM bagi pecinta alam, menjelajahi dan menjaga kelestarian lingkungan hidup.'
+                    ],
+                    [
+                        'img' => 'BGK.webp',
+                        'title' => 'Bujang Gadis Kampus(@bgk_bidar)',
+                        'desc' => 'UKM pengembangan kepribadian, etika, dan representasi duta kampus.'
+                    ],
+                    [
+                        'img' => 'futsal.webp',
+                        'title' => 'Futsal(@futsalbinadarma)',
+                        'desc' => 'UKM olahraga yang fokus pada pengembangan kemampuan dan kompetisi futsal.'
+                    ],
+                    [
+                        'img' => 'pramuka.webp',
+                        'title' => 'Pramuka(@pramuka_ubd)',
+                        'desc' => 'UKM kepramukaan yang melatih kemandirian, kepemimpinan, dan semangat kebangsaan.'
+                    ],
+                    [
+                        'img' => 'EDS.webp',
+                        'title' => 'EDS South Sumatera English Community(@eds_bidar)',
+                        'desc' => 'UKM bahasa Inggris yang fokus pada debat, public speaking, dan kompetisi English.'
+                    ],
+                    [
+                        'img' => 'basket.webp',
+                        'title' => 'Basket Club(@bidarbasketclub)',
+                        'desc' => 'UKM olahraga basket yang aktif berlatih dan mengikuti turnamen antar kampus.'
+                    ],
+                    [
+                        'img' => 'choir.webp',
+                        'title' => 'Paduan Suara Mahasiswa Universitas Bina Darma(@bidarians_choir)',
+                        'desc' => 'UKM seni suara yang menyalurkan bakat bernyanyi dan tampil di berbagai acara kampus.'
+                    ],
+                ];
+            @endphp
+
+            @foreach ($ukms as $ukm)
+                <div class="col-6 col-md-4 col-lg-3 mb-4">
+                    <div class="card h-100" style="cursor: pointer;"
+                         data-bs-toggle="modal"
+                         data-bs-target="#ukmModal"
+                         data-title="{{ $ukm['title'] }}"
+                         data-desc="{{ $ukm['desc'] }}">
+                        <img src="{{ asset('img/' . $ukm['img']) }}" class="card-img-top img-fluid" alt="{{ $ukm['title'] }}">
+                        <div class="card-body p-2">
+                            <p class="img-caption mb-1">{{ $ukm['title'] }}</p>
+                            <p class="text-muted small">Klik untuk lihat deskripsi lengkap</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/qarib.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 2">
-            <div class="card-body p-2">
-                <p class="img-caption">LDK ALQORIB(@ldkalqoribbidar)</p>
-                <p class="text-muted small">Lembaga dakwah kampus Islam yang membina rohani, akhlak, dan ukhuwah Islamiyah.</p>
+    </section>
+</div>
+
+<!-- Modal untuk Pop-up Deskripsi UKM -->
+<div class="modal fade" id="ukmModal" tabindex="-1" aria-labelledby="ukmModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ukmModalLabel">Judul UKM</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
             </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/PMKK.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 3">
-            <div class="card-body p-2">
-                <p class="img-caption">Persekutuan Mahasiswa Kristen & Katolik(@pmkkubd)</p>
-                <p class="text-muted small">Wadah pengembangan iman dan kebersamaan bagi mahasiswa Kristen dan Katolik.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/kmhdi.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 4">
-            <div class="card-body p-2">
-                <p class="img-caption">Kesatuan Mahasiswa Hindu Darma Indonesia(@kmhdipalembang)</p>
-                <p class="text-muted small">UKM keagamaan Hindu yang berfokus pada pembinaan spiritual dan budaya Hindu.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/mabidar.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 5">
-            <div class="card-body p-2">
-                <p class="img-caption">Mahasiswa Pencinta Alam(@mabidar.palembang)</p>
-                <p class="text-muted small">UKM bagi pecinta alam, menjelajahi dan menjaga kelestarian lingkungan hidup.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/BGK.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 6">
-            <div class="card-body p-2">
-                <p class="img-caption">Bujang Gadis Kampus(@bgk_bidar)</p>
-                <p class="text-muted small">UKM pengembangan kepribadian, etika, dan representasi duta kampus.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/futsal.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 7">
-            <div class="card-body p-2">
-                <p class="img-caption">Futsal(@futsalbinadarma)</p>
-                <p class="text-muted small">UKM olahraga yang fokus pada pengembangan kemampuan dan kompetisi futsal.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/pramuka.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 9">
-            <div class="card-body p-2">
-                <p class="img-caption">Pramuka(@pramuka_ubd)</p>
-                <p class="text-muted small">UKM kepramukaan yang melatih kemandirian, kepemimpinan, dan semangat kebangsaan.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/EDS.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 10">
-            <div class="card-body p-2">
-                <p class="img-caption">EDS South Sumatera English Community(@eds_bidar)</p>
-                <p class="text-muted small">UKM bahasa Inggris yang fokus pada debat, public speaking, dan kompetisi English.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/basket.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 10">
-            <div class="card-body p-2">
-                <p class="img-caption">Basket Club(@bidarbasketclub)</p>
-                <p class="text-muted small">UKM olahraga basket yang aktif berlatih dan mengikuti turnamen antar kampus.</p>
-            </div>
-        </div>
-    </div>
-     <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <div class="card h-100">
-            <img src="{{ asset('img/choir.webp') }}" class="card-img-top img-fluid" alt="Mahasiswa 10">
-            <div class="card-body p-2">
-                <p class="img-caption">Paduan Suara Mahasiswa Universitas Bina Darma(@bidarians_choir)</p>
-                <p class="text-muted small">UKM seni suara yang menyalurkan bakat bernyanyi dan tampil di berbagai acara kampus.</p>
+            <div class="modal-body" id="ukmModalDesc">
+                Deskripsi UKM akan muncul di sini.
             </div>
         </div>
     </div>
@@ -240,6 +234,20 @@
 
 @push('scripts')
     <!-- JS Libraries -->
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const ukmCards = document.querySelectorAll('.card[data-bs-toggle="modal"]');
+        const modalTitle = document.getElementById('ukmModalLabel');
+        const modalDesc = document.getElementById('ukmModalDesc');
+
+        ukmCards.forEach(card => {
+            card.addEventListener('click', function () {
+                modalTitle.textContent = this.getAttribute('data-title');
+                modalDesc.textContent = this.getAttribute('data-desc');
+            });
+        });
+    });
+</script>
 
     <!-- Page Specific JS File -->
 @endpush
